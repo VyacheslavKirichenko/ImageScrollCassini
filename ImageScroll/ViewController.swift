0a745b2f5009140bb4ab3edb3553ec09162a34d2
VyacheslavKirichenko <prostoslava321@gmail.com>
2019-04-21 06:23:18 -0700

    Initial Commit

---
+++ ImageScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageScroll
+//
+//  Created by VyacheslavKrivoi on 4/21/19.
+//  Copyright © 2019 VyacheslavKrivoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
